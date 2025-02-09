71fc9cb73f9e5727165d71a81cdd789198cbbeea
Dimitar Stoilov <dstoilov@asteasolutions.com>
2025-02-09 23:03:01 +0200

    add messages to the exceptions

--- server/src/errors.py
+++ server/src/errors.py
@@ -4,21 +4,29 @@ from fastapi import HTTPException
 
 
 class AppException(HTTPException):
+    """An default app exception"""
+
     def __init__(self, status_code: int, message: str):
         super().__init__(status_code=status_code, detail=message)
 
 
 class BadError(AppException):
+    """Exception raised for a 400 code."""
+
     def __init__(self, message: str = "Bad request"):
         super().__init__(400, message)
 
 
 class UnauthorizedError(AppException):
+    """Exception raised for a 401 code."""
+
     def __init__(self, message: str = "Unauthorized"):
         super().__init__(401, message)
 
 
 class ForbiddenError(AppException):
+    """Exception raised for 403 code."""
+
     def __init__(self, message: str = "Forbidden"):
         super().__init__(403, message)
 
